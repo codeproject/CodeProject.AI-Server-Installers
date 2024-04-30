4f0360c270d1b5f035be9fffbf9fb4e37156f0e3
Matthew Dennis <matthew.dennis50@hotmail.com>
2024-04-30 11:32:40 -0400

    Delete dist dir on build so we don't get an error on upload due to existing file

--- PyPi/CodeProject.AI-Python-SDK/build.bat
+++ PyPi/CodeProject.AI-Python-SDK/build.bat
@@ -1,3 +1,4 @@
 :: This file builds the package
 copy ..\..\..\CodeProject.AI-Python-SDK\readme.md .
+del /s /q dist
 python -m build --wheel
--- PyPi/CodeProject.AI-Python-SDK/build.sh
+++ PyPi/CodeProject.AI-Python-SDK/build.sh
@@ -1,3 +1,4 @@
 # This file builds the package
 cp ../../../CodeProject.AI-Python-SDK/readme.md .
+rm -rf dist
 python3 -m build --wheel
