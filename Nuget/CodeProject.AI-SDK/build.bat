9a3ff390f7190f71acdbe8eff597a24a80a37c11
Chris Maunder <chris.maunder@codeproject.com>
2024-11-24 23:42:18 -0500

    Updated Nuget to 1.1.0

--- Nuget/CodeProject.AI-SDK/build.bat
+++ Nuget/CodeProject.AI-SDK/build.bat
@@ -24,6 +24,6 @@ REM We're using a digicert EV Code Signing certificate that's stored on a USB
 REM drive. The USB drive's driver makes it look like the certificate is installed
 REM in windows. So just specifying the fingerprint is enough info for Windows to
 REM know where to get the certificate.
-dotnet nuget sign CodeProject.AI.Module.SDK.%NUGET_VERSION%.nupkg 
+dotnet nuget sign CodeProject.AI.Module.SDK.%NUGET_VERSION%.nupkg ^
        --certificate-fingerprint 460f1f0bb84891b110aac4fd071b6a3c2931cc2b ^
        --timestamper http://timestamp.digicert.com
